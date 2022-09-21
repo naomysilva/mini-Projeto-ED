064d2ca0d6b0e12433af7da9fa8168da8a601174
naomysilva <72279687+naomysilva@users.noreply.github.com>
2022-09-21 19:18:14 -0300

    interacao na lista sobre as acoes

--- miniProjeto.py
+++ miniProjeto.py
@@ -2,23 +2,23 @@ from queue import Empty
 
 
 class Queue:
-    
+
     def __init__(self):
         self.fila = []
-        
+
     def size(self):
         return len(self.fila)
-               
+
     def empty(self):
         if self.size() == 0:
             print("Fila Vazia")
-           
+
         else:
             print("Fila Não esta vazia")
-            
+
     def push(self,item):
         self.fila.append(item)
-        
+
     def remove(self):
         self.fila.pop(0)
 
@@ -27,15 +27,21 @@ class Queue:
            print("fila vazia")
         else:
             return len(self.fila)-1
-        
+            
+    def interacao(self):
+        for i in self.fila:
+            print("Ação:",i[0])
+
     def __repr__(self) -> str:
         return f'Queue({self.fila}' 
-        
+
 node = Queue()
-        
+
 for valor in range(3):
     acao = input(f'quantidade ações e o valor do dia {valor+1}:').split()
     node.push(acao)
     
+
+node.interacao()
 print(node) 
 print(node.size())
